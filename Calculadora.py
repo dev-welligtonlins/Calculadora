69bd5206f9a49a78c20eb80ace0583aac910658a
dev-welligtonlins <welligotnlinsemp@gmail.com>
2022-08-09 21:52:32 -0300

    2# Adicionar os operadores básicos

--- Calculadora.py
+++ Calculadora.py
@@ -3,3 +3,16 @@ number_2 = int(input('Entre com segundo número: '))
 
 print('{} + {} = '.format(number_1, number_2))
 print(number_1 + number_2)
+
+
+#subtração
+print('{} - {} = '.format(number_1, number_1))
+print(number_1 - number_2)
+
+#Multiplicação
+print('{} * {} = '.format(number_1, number_1))
+print(number_1 * number_2)
+
+#Divisão
+print('{} / {} = '.format(number_1, number_1))
+print(number_1 / number_2)
